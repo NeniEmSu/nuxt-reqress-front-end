c7d32aef740522e6de15860946ef3b1e9212c25b
cryptodoctor <emmanuelneni@gmail.com>
2020-07-13 15:18:39 +0300

    added header comp

---
+++ components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-navbar toggleable="sm" type="dark" variant="dark">
+    <b-container>
+      <b-navbar-brand to="/">
+        <b>REQ|RES</b>
+        +
+        <b-img
+          width="26px"
+          fluid
+          class="img-thumbnail"
+          src="https://nuxtjs.org/logos/nuxt-icon.png"
+        ></b-img>
+
+        Frontend</b-navbar-brand
+      >
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/users">Users</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
+</template>
+
+<style scoped>
+.nuxt-link-exact-active {
+  color: rgba(255, 255, 255, 1) !important;
+}
+</style>
