d6111f8baf0337864ea4fae70bf3feb8f9feeefc
cryptodoctor <emmanuelneni@gmail.com>
2020-07-14 01:27:49 +0300

    more page content and addeng page title and desc

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,31 @@
         Nuxt Reqress Frontend
       </h1>
       <p>
-        Front-end to consume the reqress.in dommy Api.
+        Front-end to consume the reqress.in dommy Api, with an interface that
+        allows to: View a list of users, add a new user, navigate to edit an
+        existing user, delete a user.
       </p>
+      <b-button class="btn btn-success" to="/users">Get Started!</b-button>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'HomePage',
+  head() {
+    return {
+      title: `Index`,
+      titleTemplate: '%s - Reqress Nuxt Frontend!',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'An interface that allows to: View a list of users, add a new user, navigate to edit an existing user, delete a user.',
+        },
+      ],
+    }
+  },
+}
 </script>
